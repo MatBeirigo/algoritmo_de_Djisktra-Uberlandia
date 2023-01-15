0dadfad8669e1872fe2220c0ad9496030e6d6d31
matheus beirigo <matheus.beirigo@ufu.br>
2023-01-15 18:48:03 -0300

    code

---
+++ Trabalho_final_ED/Algoritmo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_final_ED
+{
+    internal class Algoritmo
+    {
+        public static (List<string> Path, int Distance) Djisktra(Dictionary<string, Dictionary<string, int>> map, string origem, string destino)
+        {
+            try
+            {
+                // Inicializa a lista de cidades não visitadas
+                var unvisited = new HashSet<string>(map.Keys);
+
+                // Inicializa o dicionário de distâncias e o dicionário de caminhos
+                var distances = map.ToDictionary(x => x.Key, x => int.MaxValue);
+                var paths = map.ToDictionary(x => x.Key, x => new List<string>());
+
+                // Define a cidade origem como o ponto de partida
+                distances[origem] = 0;
+                paths[origem].Add(origem);
+
+                // Enquanto houver cidades não visitadas
+                while (unvisited.Any())
+                {
+                    // Seleciona a cidade com a menor distância
+                    var current = unvisited.
+                        OrderBy(x => distances[x]).First();
+                    // Remove a cidade da lista de não visitadas
+                    unvisited.Remove(current);
+                    //Para cada cidade vizinha à cidade atual
+                    foreach (var neighbor in map[current])
+                    {
+                        // Calcula a distância através da cidade atual
+                        var distance = distances[current] + neighbor.Value;
+
+                        // Se a distância for menor do que a distância atual, atualiza a distância e o caminho
+                        if (distance < distances[neighbor.Key])
+                        {
+                            distances[neighbor.Key] = distance;
+                            paths[neighbor.Key] = new List<string>(paths[current]) { neighbor.Key };
+                        }
+                    }
+                }
+
+                // Retorna o menor caminho e a distância total
+                return (paths[destino], distances[destino]);
+            }
+            catch(Exception e)
+            {
+                string erro = $"{e} \n [ERRO] , por favor execute o programa corretamente.";
+                string[] conversaoErro = erro.Split(',');
+                List<string> msgErro = new List<string>(conversaoErro);
+
+                return (msgErro,0) ;
+            }
+        }
+    }
+}
---
+++ Trabalho_final_ED/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_final_ED
+{
+    internal class Program : Algoritmo
+    {
+        static void Main(string[] args)
+        {
+            // Mapa de distâncias entre cidades (em km)
+            Dictionary<string, Dictionary<string, int>> map = new Dictionary<string, Dictionary<string, int>>
+        {
+            { "Itumbiara", new Dictionary<string, int> { { "Tupaciguara", 55}, { "Centralina", 40 } } },
+            { "Centralina", new Dictionary<string, int> { { "Capinópolis", 40 }, { "Monte Alegre de Minas", 75 } } },
+            { "Tupaciguara", new Dictionary<string, int> { { "Monte Alegre de Minas", 44 }, { "Itumbiara", 55 }, { "Uberlândia", 60 } } },
+            { "Capinópolis", new Dictionary<string, int> { { "Centralina", 40 }, { "Ituiutaba", 30 } } },
+            { "Ituiutaba", new Dictionary<string, int> { { "Capinópolis", 30 }, { "Douradinhos", 90 }, { "Monte Alegre de Minas", 85 } } },
+            { "Douradinhos", new Dictionary<string, int> { { "Ituiutaba", 90 }, { "Monte Alegre de Minas", 28 }, { "Uberlândia", 63 } } },
+            { "Monte Alegre de Minas", new Dictionary<string, int> { { "Ituiutaba", 85 }, { "Douradinhos", 28 }, { "Centralina", 75 }, { "Tupaciguara", 44 }, { "Uberlândia", 60 } } },
+            { "Uberlândia", new Dictionary<string, int> { { "Tupaciguara", 60 }, { "Monte Alegre de Minas", 60 }, { "Douradinhos", 63 }, { "Araguari", 30 }, { "Indianópolis", 45 }, { "Romaria", 78 } } },
+            { "Araguari", new Dictionary<string, int> { { "Uberlândia", 30 }, { "Estrela do Sul", 34 }, { "Cascalho Rico", 28 } } },
+            { "Cascalho Rico", new Dictionary<string, int> { { "Araguari", 28 }, { "Grupiara", 32 } } },
+            { "Grupiara", new Dictionary<string, int> { { "Cascalho Rico", 32 }, { "Estrela do Sul", 38 } } },
+            { "Estrela do Sul", new Dictionary<string, int> { { "Araguari", 34 }, { "Grupiara", 38 }, { "Romaria", 27 } } },
+            { "Romaria", new Dictionary<string, int> { { "Estrela do Sul", 27 }, { "Uberlândia", 78 }, { "São Juliana", 28 } } },
+            { "Indianópolis", new Dictionary<string, int> { { "Uberlândia", 45 }, { "São Juliana", 40 } } },
+            { "São Juliana", new Dictionary<string, int> { { "Indianópolis", 40 }, { "Romaria", 28 } } }
+        };
+
+            // Cidade origem e destino selecionadas pelo usuário
+            Console.WriteLine("Insira a cidade de origem:");
+            string origem = Console.ReadLine();
+            Console.WriteLine("Insira a cidade de destino:");
+            string destino = Console.ReadLine();
+
+            // Executa o algoritmo de Djisktra
+            var result = Djisktra(map, origem, destino);
+            Console.WriteLine("----------------------------------------");
+            // Imprime o menor caminho e a distância total
+            Console.WriteLine("Menor caminho: " + string.Join(" -> ", result.Path));
+            Console.WriteLine("Distância total: " + result.Distance + " km"); ;
+            Console.ReadLine();
+        }
+
+        
+    }
+}
+    
+
